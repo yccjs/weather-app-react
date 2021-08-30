04a5f221a5d1bb7a764f6cf6a87f35b99ddf3e81
yccjs <84162315+yccjs@users.noreply.github.com>
2021-08-30 10:25:01 +0900

    Added a footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      hi!  </div>
+     <h1> Weather App</h1>
+      
+      <footer> This project was coded by Yushiko Cloe Salvador and is 
+        <a href="https://github.com/yccjs/weather-app-react"> open-sourced on GitHub.
+          
+          </a></footer>  </div>
   );
 }
 
