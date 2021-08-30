e11baadc4668853ae5e7f10fe08e2a385e2094d6
yccjs <84162315+yccjs@users.noreply.github.com>
2021-08-30 10:39:51 +0900

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1> Weather App</h1>
       
       <footer> This project was coded by Yushiko Cloe Salvador and is 
         <a href="https://github.com/yccjs/weather-app-react"> open-sourced on GitHub.
           
-          </a></footer>  </div>
+          </a></footer>  </div></div>
   );
 }
 
