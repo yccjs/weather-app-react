66140376cb35de5e7d78324ce6c6721a4047fcc6
yccjs <84162315+yccjs@users.noreply.github.com>
2021-08-31 11:59:19 +0900

    Added CSS to my project

--- src/Weather.js
+++ src/Weather.js
@@ -3,18 +3,22 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="weather">
-<form>
-    <div className="row">
-        <div className="col-9">
-<input type="search" placeholder="Input a city" className="form-control"/>
-</div>
-<div className="col-3">
-<input type="submit" value="Search" className="btn btn-info"/>
-
-       </div>
-    </div>
-</form>
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Input a city"
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-info " />
+          </div>
+        </div>
+      </form>
 
       <h1> Tokyo</h1>
       <ul>
@@ -27,14 +31,17 @@ export default function Weather() {
           <img
             src="https://cdn3.iconfinder.com/data/icons/lucid-weather/24/sun_hot_brightness_sunny_star-512.png"
             alt="sunny"
-            width="100px"
+            
           />{" "}
-          32°
+          <span className="temperature">16</span>{" "}
+          <span className="celsius">°C</span>{" "}
+          <span className="division">|</span>{" "}
+          <span className="fahrenheit">°F</span>
         </div>
 
         <div className="col-6">
           <ul>
-            <li>Precipitatiom: 15%</li>
+            <li>Precipitation: 15%</li>
             <li>Humidity: 72%</li>
             <li>Wind: 16 km/h</li>
           </ul>
